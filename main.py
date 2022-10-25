01a5b44eeff3692a0f9e7f8cd2a89dfdb5a4d9b6
angelomorgado <angelo.morgado@ubi.pt>
2022-10-25 19:21:11 +0100

    Created a notebook of the project

--- main.py
+++ main.py
@@ -46,30 +46,30 @@ def remove_outliers(data, threshold=20.0):
 
 # Plot the ROC curve
 def plot_roc_curve(classifiers, classifiersNames, X_test, y_test):
-        # Compute ROC curve and ROC area for each class
-        fpr = dict()
-        tpr = dict()
-        roc_auc = dict()
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
 
-        for classifier, name in zip(classifiers, classifiersNames):
-            y_pred = classifier.predict(X_test)
-            fpr[name], tpr[name], _ = roc_curve(y_test, y_pred)
-            roc_auc[name] = auc(fpr[name], tpr[name])
+    for classifier, name in zip(classifiers, classifiersNames):
+        y_pred = classifier.predict(X_test)
+        fpr[name], tpr[name], _ = roc_curve(y_test, y_pred) 
+        roc_auc[name] = auc(fpr[name], tpr[name])
 
-        # Plot all ROC curves
-        plt.figure()
-        lw = 2
-        for name in classifiersNames:
-            plt.plot(fpr[name], tpr[name], lw=lw, label=name + ' (area = %0.2f)' % roc_auc[name])
-        
-        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('Receiver operating characteristic')
-        plt.legend(loc="lower right")
-        plt.savefig('ROC.png')
+    # Plot all ROC curves
+    plt.figure()
+    lw = 2
+    for name in classifiersNames:
+        plt.plot(fpr[name], tpr[name], lw=lw, label=name + ' (area = %0.2f)' % roc_auc[name])
+    
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig('ROC.png')
 
 # Chose the best classifier using f1 score
 def get_best_classifier(classifiers, classifiersNames, X_test, y_test):
@@ -106,14 +106,14 @@ if __name__ == "__main__":
     # Remove outliers
     data = remove_outliers(data, 23.0)
 
+    # Deal with missing values in dataset (replace with mean value of rows with the same y (label) value)) 
+    data = data.fillna(data.groupby('y').transform('mean'))
+
     # Split dataset into features and labels
     X = data[['mntcm', 'mxtwm', 'rfseas', 'tann']]
     y = data['y']
 
-    # Deal with missing values in dataset (replace with mean)
-    X = X.fillna(X.mean())
-
-    # Split the dataset randomly into training and testing using cross validation
+    # Split the dataset randomly into training and testing
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     # Standardize features values to be between 0 and 1 (for better performance)
@@ -136,17 +136,20 @@ if __name__ == "__main__":
     for classifier in classifiers:
         classifier.fit(X_train, y_train)
 
-    # Evaluate each classifier using cross validation and f1 score (to avoid overfitting)
+    # Evaluate each classifier using cross validation and f1 score
     skf = StratifiedKFold(n_splits=10, shuffle=True)
     for classifier, name in zip(classifiers, classifiersNames):
         scores = cross_val_score(classifier, X, y, cv=skf) # cross_val_score returns the f1 score for each fold of the cross validation (10 folds) 
         print(name + ' : ' + str(scores.mean())) # Print the mean of the f1 scores
+
+    # TODO: Fazer histograma com os scores de cada classificador
 
     # Plot the ROC curve
     plot_roc_curve(classifiers, classifiersNames, X_test, y_test)
 
     # Chose the best classifier using f1 score
     bestClassifier = get_best_classifier(classifiers, classifiersNames, X_test, y_test)
+    # TODO: Fazer histograma com os scores de cada classificador
 
     # Predict using the best classifier
     y_pred = bestClassifier.predict(X_test)
